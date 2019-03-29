fb17a728e7ab2fe61191bd5589db00baaf8e1024
Benoît Ganne <benoit.ganne@gmail.com>
2019-03-29 15:04:39 +0100

    fix setup

--- setup.sh
+++ setup.sh
@@ -3,5 +3,5 @@ set -eu
 FILES="gdbinit gitconfig gitignore inputrc screenrc vim vimrc"
 MYDIR="$(dirname "$0")"
 for f in $FILES; do
-    ln -vrs "$@" "$MYDIR/$f" "$HOME/.$f"
+    ln -Tvrs "$@" "$MYDIR/$f" "$HOME/.$f"
 done

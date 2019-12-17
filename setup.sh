fe614c45043400a80cc96bafe183716746e4b03f
Benoît Ganne <benoit.ganne@gmail.com>
2019-12-17 18:01:36 +0100

    add profile

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eu
-FILES="gdbinit gitconfig gitignore inputrc screenrc vim vimrc"
+FILES="gdbinit gitconfig gitignore inputrc profile screenrc vim vimrc"
 MYDIR="$(dirname "$0")"
 for f in $FILES; do
-    ln -Tvrs "$@" "$MYDIR/$f" "$HOME/.$f"
+    ln -fTvrs "$@" "$MYDIR/$f" "$HOME/.$f"
 done
